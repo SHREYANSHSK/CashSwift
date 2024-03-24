1ef02e5ad5c11c3d2cc460eec6176d465e02dc2b
Shreyansh Khandelwal <khandelwal.happy.hk@gmail.com>
2024-03-24 12:16:08 +0530

    initial commit

---
+++ lib/home_Page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class home_Page extends StatefulWidget {
+  const home_Page({super.key});
+
+  @override
+  State<home_Page> createState() => _home_PageState();
+}
+
+class _home_PageState extends State<home_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:cash_swift/home_Page.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Money Transfer',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: home_Page(),
+    );
+  }
+}
